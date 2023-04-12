c323cb0c7b8375f342418233974751760d07686f
Danny <dnnndungu@gmail.com>
2023-04-12 17:43:37 +0300

    write on laptops.txt
    output contents to a text file

--- scrape jumia.py
+++ scrape jumia.py
@@ -5,13 +5,19 @@ htmldoc=requests.get('https://www.jumia.co.ke/laptops/')
 soup=BeautifulSoup(htmldoc.text,'lxml')
 a=[]
 b=[]
-for category in soup.findAll("div",class_="crs row _no-g -fw-nw _6cl-4cm -pvxs"):
-    for laptops in category.findAll('div',class_='name'):
-        c=(laptops.text)
-        a.append(c)
-    for prices in category.findAll("div",class_="prc"):
-        d=(prices.text)
-        b.append(d)
-    for a,b in zip(a,b):
-        e=a+b
-        print(e)  
+with open('laptops.txt', 'w', encoding='utf-8') as f:
+    
+    for category in soup.find("div",class_="crs-w _main -phxs"):
+        for all in category.findAll('a',class_='core'):
+                
+            for laptops in category.findAll('div',class_='name'):
+                c =(laptops.text)
+                a.append(c)
+            for prices in category.findAll("div",class_="prc"):
+                d=(prices.text)
+                b.append(d)
+        
+            with open('laptops.txt', 'w', encoding='utf-8') as f:
+                    for a,b in zip(a,b):                
+                        f.write(a+b)
+                        print(a+b) 
